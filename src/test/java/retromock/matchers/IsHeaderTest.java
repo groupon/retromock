8f2fc1ff32c68c7150d5bb0d5b778e43414def24
Bogdan Langendorf <bogdan.langendorf@groupon.com>
2014-06-17 14:54:18 +0200

    UnitTest isHeader

---
+++ src/test/java/retromock/matchers/IsHeaderTest.java
@@ -0,0 +1,28 @@
+package retromock.matchers;
+
+import org.hamcrest.Matcher;
+import org.junit.Test;
+import retrofit.client.Header;
+
+import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.text.IsEmptyString.isEmptyOrNullString;
+
+import static org.junit.Assert.*;
+
+public class IsHeaderTest {
+
+    static final Matcher<Header> isHeader = IsHeader.header("name", not(isEmptyOrNullString()));
+
+    @Test
+    public void testIsHeaderTrue() throws Exception {
+        Header header = new Header("name", "value");
+        assertTrue(isHeader.matches(header));
+    }
+
+    @Test
+    public void testIsHeaderFalse() throws Exception {
+        Header header = new Header("name", "");
+        assertFalse(isHeader.matches(header));
+    }
+
+}
