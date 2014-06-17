ffedbb42632047ea2e55ac334b0044a7be69f8f0
Bogdan Langendorf <bogdan.langendorf@groupon.com>
2014-06-17 13:47:57 +0200

    Tweak parametrized emptyList

--- src/test/java/retromock/MockClientTest.java
+++ src/test/java/retromock/MockClientTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import retrofit.RestAdapter;
 import retrofit.client.Request;
 import retrofit.client.Response;
+import retrofit.client.Header;
 import retrofit.http.Body;
 import retrofit.http.GET;
 import retrofit.http.Headers;
@@ -28,13 +29,13 @@ public class MockClientTest {
 
     @Test
     public void testARequest() throws Exception {
-        Response getResponse = new Response("", 200, "", Collections.EMPTY_LIST, new TypedByteArray("text/plain", "\"Hello, World\"".getBytes()));
+        Response getResponse = new Response("", 200, "", Collections.<Header>emptyList(), new TypedByteArray("text/plain", "\"Hello, World\"".getBytes()));
         final MockClient.ResponseFactory responseFactory = new MockClient.ResponseFactory() {
             @Override
             public Response createFrom(Request request) throws IOException {
                 ByteArrayOutputStream os = new ByteArrayOutputStream();
                 request.getBody().writeTo(os);
-                return new Response("", 200, "", Collections.EMPTY_LIST, new TypedByteArray("text/plain", os.toByteArray()));
+                return new Response("", 200, "", Collections.<Header>emptyList(), new TypedByteArray("text/plain", os.toByteArray()));
             }
         };
         MockClient.Provider client = MockClient.when()
